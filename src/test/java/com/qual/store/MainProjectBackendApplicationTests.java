8fc1227e9529fca7c2e8de790032afe0e99e29ce
Stefania Vintilescu <stefania.vintilescu@yahoo.com>
2024-02-26 21:36:23 +0200

    Init Commit

---
+++ src/test/java/com/qual/store/MainProjectBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.qual.store;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class MainProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
